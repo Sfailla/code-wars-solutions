dd6402f3ad5f1cd7b3ab98002d9fccd542e69560
Stevo99 <Sfailla1983@gmail.com>
2019-11-15 10:41:45 -0500

    added creative answer

--- kata6/weirdStringCase.js
+++ kata6/weirdStringCase.js
@@ -1,21 +1,47 @@
+/*
+uppercase every even indexed letter. If there is more than one word, the new words Index starts at zero
+
+toWeirdCase( "String" );//=> returns "StRiNg"
+toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe"
+
+creative answer
+*/
+function toWeirdCase(string) {
+	return string
+		.split(' ')
+		.map(function(word) {
+			return word
+				.split('')
+				.map(function(letter, index) {
+					return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase();
+				})
+				.join('');
+		})
+		.join(' ');
+}
+
 function toWeirdCase(string) {
 	// TODO
-	let count = 0;
-	count -= 1;
-	string = string.split('');
+
+	let words = [];
+	let array = [];
+
+	string = string.split(' ');
 	string.map((word, index) => {
-		if (word === ' ') {
-			count = 0;
-		} else {
-			count++;
-		}
-		console.log();
-		// for (let i = 0; i < word.length; i++) {
-		// 	console.log(word[i]);
-		// }
-		// if (count % 2 === 0) {
-		// }
+		let split = word.split('');
+		words.push(split);
 	});
+
+	for (let i = 0; i < words.length; i++) {
+		words[i].map((letter, index) => {
+			if (index === 0) {
+				array.push(' ');
+			}
+			index % 2 === 0 ? array.push(letter.toUpperCase()) : array.push(letter);
+		});
+	}
+	let answer = array.slice(1).join('');
+	return answer;
 }
 
 toWeirdCase('Weird string case');
