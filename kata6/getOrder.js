70f12c38c7242c1ad911475b74215e9ba9d94df6
Stevo99 <Sfailla1983@gmail.com>
2019-11-20 10:41:40 -0500

    improvement to code answer

--- kata6/getOrder.js
+++ kata6/getOrder.js
@@ -10,12 +10,27 @@ function getOrder(input) {
 		'Coke'
 	];
 
-	let orders = input.match(new RegExp(menu.join('|'), 'gi'))
-	  .map(item => item[0].toUpperCase() + item.slice(1))
-	  .sort((a, b) => menu.indexOf(a) - menu.indexOf(b));
+	let array = [];
 
-	return orders.join(' ');
+	let orders = input
+		.match(new RegExp(menu.join('|'), 'gi'))
+		.map(item => item[0].toUpperCase() + item.slice(1))
+		.sort((a, b) => menu.indexOf(a) - menu.indexOf(b));
+
+	orders.map(val => {
+		if (array.indexOf(val) === -1) {
+			array.push(val);
+		}
+	});
+
+	return array.join(' ');
 }
 
-getOrder('milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza');
-console.log(getOrder('milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza'));
+getOrder(
+	'milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza'
+);
+console.log(
+	getOrder(
+		'milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza'
+	)
+);
