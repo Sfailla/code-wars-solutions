b27ad078921dcecf45f4b241c2d09eeb34e5e583
Stevo99 <Sfailla1983@gmail.com>
2019-11-29 15:51:22 -0500

    working on solution for kata7

--- kata7/growingPlant.js
+++ kata7/growingPlant.js
@@ -1,10 +1,28 @@
 function growingPlant(upSpeed, downSpeed, desiredHeight) {
 	//coding and coding..
+	// let formula;
+	// upSpeed >= desiredHeight
+	// 	? (formula = 1)
+	// 	: (formula = desiredHeight / (upSpeed - downSpeed));
+
+	// let answer = parseInt(formula.toFixed(0));
+
 	let days = 0;
 	let total = 0;
-	let formula = desiredHeight / (upSpeed - downSpeed);
+	let answer;
 
-	return formula.toFixed(0);
+	while (total < desiredHeight) {
+		if (total + upSpeed === desiredHeight) {
+			return total;
+		} else if (days === 40) {
+			break;
+		}
+		days++;
+		answer = total + (upSpeed - downSpeed);
+	}
+	console.log(answer);
+
+	return answer;
 }
 
-console.log(growingPlant(100, 10, 910)); // should return 10
+console.log(growingPlant(46, 45, 165)); // should return 120
