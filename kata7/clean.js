f4a1bac99b488cc14a2cfc19fdbc1e3b5de007a2
Stevo99 <Sfailla1983@gmail.com>
2019-11-30 16:18:57 -0500

    added comments for solution

--- kata7/clean.js
+++ kata7/clean.js
@@ -1,11 +1,14 @@
 function clean(arr) {
+	// set empty array to store our array values that we want to keep
 	let array = [];
-
+	// filter array and remove any empty spaces
 	arr.filter(val => {
+		// if array item is not blank we push to our placeholder array
 		if (val !== '') {
 			array.push(val);
 		}
 	});
+	// finally return array with the valid numbers only
 	return array;
 }
 
