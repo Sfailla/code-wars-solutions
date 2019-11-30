b3b180208f0a4835d8c5a380ba11041c4f713468
Stevo99 <Sfailla1983@gmail.com>
2019-11-30 16:33:45 -0500

    added solution and comments for kata7

--- kata7/howManyArgs.js
+++ kata7/howManyArgs.js
@@ -1,6 +1,6 @@
-function args_count() {
-	let length = array.length;
-	return length;
+function args_count(...args) {
+	// spread the arguments and return the length. ES6
+	return args.length;
 }
 
 args_count('A', 'B', 'C'); // 5
