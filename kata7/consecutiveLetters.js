86df2c577714cc03b46a759b97b4d28de1bcb035
Stevo99 <Sfailla1983@gmail.com>
2019-11-30 16:19:13 -0500

    solution for kata7

--- kata7/consecutiveLetters.js
+++ kata7/consecutiveLetters.js
@@ -14,14 +14,41 @@ solve("v") = True
 
 const solve = s => {
 	// solve
-	let sorted = [ ...s ].sort();
 
-	sorted.filter((letter, idx) => {
-		// console.log(idx, letter);
-		console.log(answer);
-		return answer;
+	let array = [];
+	let charArr = [];
+	let charCodeArr = [];
+	let charToCode;
+	let sorted = [ ...s ].sort();
+	// first sort the array and check if any duplicates
+	sorted.map((letter, idx) => {
+		// if sorted array has duplicate we push that duplicate to array
+		if (sorted.indexOf(letter) !== idx) {
+			array.push(letter);
+		}
 	});
+	// if the array has length there is a duplicate and we return false
+	if (array.length) {
+		return false;
+	}
+	// otherwise we loop thru sorted array and turn char to char code values.
+	sorted.map((letter, idx) => {
+		charToCode = sorted.join('').charCodeAt(idx);
+		charArr.push(charToCode);
+	});
+	// grab the lowest number and highest number in array range
+	let max = Math.max(...charArr);
+	let min = Math.min(...charArr);
+	// loop thru array and push the range of ascii numbers to an array
+	for (let i = min; i <= max; i++) {
+		charCodeArr.push(i);
+	}
+	// if the array range is equal to sorted array we can return true.  they match!!
+	if (charArr.join('') === charCodeArr.join('')) {
+		return true;
+	}
+	// else we can return false
+	return false;
 };
 
-solve('acdba');
-console.log(solve('acdba'));
+console.log(solve('acadb')); // false a repeats itself
