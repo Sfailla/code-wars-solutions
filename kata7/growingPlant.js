1acde88a17d15950a6aa5c837096f140b4fe061d
Stevo99 <Sfailla1983@gmail.com>
2019-11-30 14:34:50 -0500

    solved kata7 algorithm

--- kata7/growingPlant.js
+++ kata7/growingPlant.js
@@ -1,28 +1,22 @@
 function growingPlant(upSpeed, downSpeed, desiredHeight) {
-	//coding and coding..
-	// let formula;
-	// upSpeed >= desiredHeight
-	// 	? (formula = 1)
-	// 	: (formula = desiredHeight / (upSpeed - downSpeed));
-
-	// let answer = parseInt(formula.toFixed(0));
-
+	// set 2 vars to keep track of the totalHeight and the number of days
 	let days = 0;
 	let total = 0;
-	let answer;
 
 	while (total < desiredHeight) {
-		if (total + upSpeed === desiredHeight) {
-			return total;
-		} else if (days === 40) {
-			break;
-		}
+		// while the total < desiredHeight we add the total to the upSpeed
+		total += upSpeed;
+		// if total < desiredHeight we add another day and repeat process
 		days++;
-		answer = total + (upSpeed - downSpeed);
+		// when total is equal or above desiredHeight we return the day counter
+		if (total >= desiredHeight) {
+			return days;
+		}
+		// else we subtract the downspeed from total and start another loop
+		total -= downSpeed;
 	}
-	console.log(answer);
-
-	return answer;
+	// finally return the answer
+	return days;
 }
 
 console.log(growingPlant(46, 45, 165)); // should return 120
