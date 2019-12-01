941ac393ac1ef6d9a3d25fbf025abb79d8d8b7b8
Stevo99 <Sfailla1983@gmail.com>
2019-11-30 22:31:46 -0500

    solution for kata7

---
+++ kata7/sumOfOddNum.js
@@ -0,0 +1,38 @@
+function rowSumOddNumbers(n) {
+	// TODO
+	let num = 10000;
+	let oddNum = [];
+	let arr = [];
+
+	for (let i = 0; i <= num; i++) {
+		if (i % 2 !== 0) {
+			oddNum.push(i);
+		}
+	}
+
+	oddNum.map((val, idx) => {
+		arr.push(oddNum.splice(0, idx + 1));
+		return arr;
+	});
+
+	let reduced = arr.reduce((acc, val, i) => {
+		let idx = i + 1;
+		acc[idx] = val;
+		return acc;
+	}, {});
+
+	let realObj = Object.assign({}, reduced);
+	if (typeof n === 'number') {
+		let result = realObj[n];
+
+		let answer = result.reduce((acc, val) => {
+			return acc + val;
+		}, 0);
+
+		return answer;
+	} else {
+		return 'not a number';
+	}
+}
+
+console.log(rowSumOddNumbers('s')); // 27
