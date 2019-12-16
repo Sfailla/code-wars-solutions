d4e67a5cb68ce18ddf3921c9dbad219ea4b1f307
Stevo99 <Sfailla1983@gmail.com>
2019-12-16 15:51:46 -0500

    solution for kata6

--- kata6/MostMoney.js
+++ kata6/MostMoney.js
@@ -17,19 +17,90 @@ const geoff = new Student('Geoff', 0, 3, 0);
 
 function mostMoney(students) {
 	// code here
+	function name(person) {
+		return person.name;
+	}
 
-	// let name = students[1].name;
-	// let fives = students[0].fives
-	// let tens = students[0].tens;
-	// let twenties = students[0].twenties;
+	function fives(person) {
+		return person.fives * 5;
+	}
 
-	if (students.length <= 1) return students[0].name;
+	function tens(person) {
+		return person.tens * 10;
+	}
+
+	function twenties(person) {
+		return person.twenties * 20;
+	}
+
+	if (students.length <= 1) return name(students[0]);
+
+	const studentName = [];
 
 	for (let i = 0; i < students.length; i++) {
 		let element = students[i];
+
+		const group = {
+			name: name(element),
+			fives: fives(element),
+			tens: tens(element),
+			twenties: twenties(element)
+		};
+
+		studentName.push(group);
 	}
 
-	return students;
+	const total = [];
+
+	function getTotal() {
+		studentName.map(val => {
+			const obj = {
+				name: val.name,
+				total: val.fives + val.tens + val.twenties
+			};
+
+			total.push(obj);
+		});
+		return total;
+	}
+
+	const totals = getTotal();
+	totals;
+
+	const reduced = totals.reduce((acc, val) => {
+		acc[val.name] = val.total;
+		return acc;
+	}, {});
+
+	const arr = [];
+	const maxTotal = [];
+
+	for (const [ key, val ] of Object.entries(reduced)) {
+		arr.push(val);
+	}
+
+	const sort = arr.sort((a, b) => b - a);
+	sort;
+	const max = sort[0];
+
+	for (const [ key, val ] of Object.entries(reduced)) {
+		key;
+		val;
+
+		if (val == max) {
+			maxTotal.push(key);
+		}
+	}
+
+	let answer = maxTotal;
+
+	if (answer.length > 1) {
+		answer = 'all';
+	} else {
+		answer = answer.join('');
+	}
+	answer;
+	return answer;
 }
 
 console.log(mostMoney([ andy, stephen, eric, david, phil ]), 'Eric');
@@ -42,6 +113,6 @@ console.log(
 console.log(mostMoney([ andy ]), 'Andy');
 console.log(mostMoney([ stephen ]), 'Stephen');
 
-// "What happens if all students have the same amount of money?"
+('What happens if all students have the same amount of money?');
 console.log(mostMoney([ cam, geoff ]), 'all');
 console.log(mostMoney([ david, cam, geoff ]), 'all');
