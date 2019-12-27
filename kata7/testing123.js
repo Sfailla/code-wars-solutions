dd8df4bdb0d6e0e2377c325ed663a92c44c66573
Stevo99 <Sfailla1983@gmail.com>
2019-12-27 18:42:17 -0500

    solution for testing123

--- kata7/testing123.js
+++ kata7/testing123.js
@@ -1,10 +1,18 @@
 const number = function(array) {
-	const mapped = array.map(val => {
-		console.log(val);
-		return val;
-	});
+	const newArray = [];
 
-	return mapped;
+	for (let i = 0; i < array.length; i++) {
+		const element = array[i];
+		const strReturn = `${i + 1}: ${element}`;
+		newArray.push(strReturn);
+	}
+
+	return newArray;
 };
 
-console.log(number([ 'a', 'b', 'c' ]));
+console.log(number([]), [], 'Empty array should return empty array');
+console.log(
+	number([ 'a', 'b', 'c' ]),
+	[ '1: a', '2: b', '3: c' ],
+	'Return the correct line numbers'
+);
