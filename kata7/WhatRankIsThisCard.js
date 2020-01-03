848c1c7c34e36271d8f9aa756dc42bdc03ec7127
Stevo99 <Sfailla1983@gmail.com>
2020-01-02 22:28:31 -0500

    solution for kata7

---
+++ kata7/WhatRankIsThisCard.js
@@ -0,0 +1,46 @@
+function rank(card) {
+	function splitToCorrectArrayValues(input) {
+		const split = input.split('');
+		const cardType = split[0];
+		const suite = split[1];
+
+		return {
+			cardType,
+			suite
+		};
+	}
+
+	function getCardValue(cardVal, suiteVal) {
+		const isNumber = /[0-9]/g;
+		const isLetter = /[A-Z]/g;
+
+		const cardValues = {
+			T: 10,
+			J: 11,
+			Q: 12,
+			K: 13,
+			A: 14
+		};
+
+		if (cardVal < 2) return 0;
+		if (cardVal.match(isNumber) && cardVal > 1) return cardVal;
+		if (cardVal.match(isLetter)) {
+			if (Object.keys(cardValues).includes(cardVal)) {
+				return cardValues[cardVal];
+			}
+		}
+
+		return 0;
+	}
+
+	const { cardType, suite } = splitToCorrectArrayValues(card);
+	const answer = parseInt(getCardValue(cardType, suite));
+
+	return answer;
+}
+
+console.log(rank('2s'), 2);
+console.log(rank('7h'), 7);
+console.log(rank('Jc'), 11);
+console.log(rank('Xd'), 14);
+console.log(rank('1d'), 0);
