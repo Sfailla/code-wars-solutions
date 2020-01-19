6819f07f936466ce5ba941df2a66273886c2b9f7
Stevo99 <Sfailla1983@gmail.com>
2020-01-18 22:05:12 -0500

    solution for kata 7

--- kata7/makeASquareBox.js
+++ kata7/makeASquareBox.js
@@ -1,37 +1,76 @@
 function box(n) {
 	//your code here
-	let num = n;
-	// main peices are - * n
-	// side peices are n - 2;
 
 	function boxTop(size) {
 		let b = '';
-
 		while (size > 0) {
 			b += '-';
-			n--;
+			size--;
 		}
-
 		return b;
 	}
 
 	function boxSide(size) {
+		let num = size;
 		let c = '';
-
-		while (size > 0) {
+		while (num > 0) {
 			c += '-';
-			n--;
+			num--;
 		}
-
-		return c;
+		const split = c.split('');
+		const map = split.map((s, i) => {
+			if (i > 0 && i < split.length - 1) {
+				s = ' ';
+				return s;
+			} else {
+				return s;
+			}
+		});
+		return map.join('');
 	}
 
-	boxTop();
-	boxSide();
+	function amountOfSides(boxSide) {
+		const array = [];
+		let times = num - 2;
+		while (times > 0) {
+			array.push(boxSide);
+			times--;
+		}
+		return array;
+	}
+
+	const num = n;
+	const top = boxTop(num);
+	const bottom = boxTop(num);
+	const side = boxSide(num);
+
+	const boxArray = [ top, amountOfSides(side), bottom ];
+	const finalArray = boxArray.reduce(
+		(acc, item) => acc.concat(item),
+		[]
+	);
+
+	return finalArray;
+}
+
+// very clever solution
+function box(n) {
+	return [
+		'-'.repeat(n),
+		...Array(n - 2).fill('-' + ' '.repeat(n - 2) + '-'),
+		'-'.repeat(n)
+	];
 }
 
 console.log(box(5), [ '-----', '-   -', '-   -', '-   -', '-----' ]);
-// console.log(box(2), ["--", "--"]);
-// console.log(box(4), ["----", "-  -", "-  -", "----"]);
-// console.log(box(6), ["------", "-    -", "-    -", "-    -", "-    -", "------"]);
-// console.log(box(3), ["---", "- -", "---"]);
+console.log(box(2), [ '--', '--' ]);
+console.log(box(4), [ '----', '-  -', '-  -', '----' ]);
+console.log(box(6), [
+	'------',
+	'-    -',
+	'-    -',
+	'-    -',
+	'-    -',
+	'------'
+]);
+console.log(box(3), [ '---', '- -', '---' ]);
