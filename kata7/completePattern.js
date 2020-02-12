4bfe9d1e29daa5133b68c1227a37f9998e2bc0c4
Stevo99 <Sfailla1983@gmail.com>
2020-02-12 09:32:41 -0500

    working on solution for kata 7

--- kata7/completePattern.js
+++ kata7/completePattern.js
@@ -1,30 +1,15 @@
 function pattern(n) {
-	var output = '';
-	let arr = [];
+	let output = '';
+	const arr = [];
 
 	for (let i = 1; i <= n; i++) {
-		arr.push(i);
+		output = i;
+		arr.push(output.toString().repeat(i));
 	}
-
-	let i = 1;
-	while (i <= arr.length) {
-		output += i;
-		i++;
-	}
-
-	output;
-
-	arr;
-
-	let count = 1;
-	const map = arr.map((val, i) => {
-		i = i + 1;
-		return Array(i).fill(i);
-	});
-
-	// return output
+	const joined = arr.join('\n');
+	return joined;
 }
 
-// console.log(pattern(1),"1");
-// console.log(pattern(2),"1\n22");
+console.log(pattern(1), '1');
+console.log(pattern(2), '1\n22');
 console.log(pattern(5), '1\n22\n333\n4444\n55555');
