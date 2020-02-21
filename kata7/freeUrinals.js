795738549d1185ac1dc7ef79b605f0ee475be0ea
Stevo99 <Sfailla1983@gmail.com>
2020-02-21 11:01:09 -0500

    solution for kata 7

---
+++ kata7/freeUrinals.js
@@ -0,0 +1,16 @@
+function getFreeUrinals(urinals) {
+	// very difficult for now ill come back;
+
+	return urinals;
+}
+
+console.log(getFreeUrinals('10001'), 1);
+console.log(getFreeUrinals('1001'), 0);
+console.log(getFreeUrinals('00000'), 3);
+console.log(getFreeUrinals('0000'), 2);
+console.log(getFreeUrinals('01000'), 1);
+console.log(getFreeUrinals('00010'), 1);
+console.log(getFreeUrinals('10000'), 2);
+console.log(getFreeUrinals('1'), 0);
+console.log(getFreeUrinals('0'), 1);
+console.log(getFreeUrinals('10'), 0);
