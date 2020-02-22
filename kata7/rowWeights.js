4e6dd016b273bffe24f8291debbaaf9040b87c1b
Stevo99 <Sfailla1983@gmail.com>
2020-02-22 12:16:46 -0500

    solution for kata 7

--- kata7/rowWeights.js
+++ kata7/rowWeights.js
@@ -2,36 +2,14 @@ function rowWeights(array) {
 	let evenArr = [];
 	let oddArr = [];
 
-	array.map((items, idx) => {
-		let even = idx % 2 === 0;
-		let odd = idx % 2 === 1;
+	const getTotal = arr => arr.reduce((acc, val) => acc + val, 0);
 
-		if (even) evenArr.push(items);
-		if (odd) oddArr.push(items);
+	array.map((val, idx) => {
+		return idx % 2 === 0 ? evenArr.push(val) : oddArr.push(val);
 	});
 
-	if (evenArr.length === 1) {
-		evenArr = [ evenArr[0], 0 ];
-	}
-	if (oddArr.length === 1) {
-		oddArr = [ 0, oddArr[0] ];
-	}
-
-	console.log(oddArr);
-
-	let evenTotal = evenArr.reduce((acc, idx) => {
-		acc + idx;
-	}, 0);
-
-	let oddTotal = oddArr.reduce((acc, idx) => {
-		acc + idx;
-	}, 0);
-
-	console.log(evenTotal);
-	// console.log([ evenTotal, oddTotal ]);
-	// return [ evenTotal, oddTotal ];
+	return [ getTotal(evenArr), getTotal(oddArr) ];
 }
 
 // rowWeights([ 29, 83, 67, 53, 19, 28, 96 ]);
-
-rowWeights([ 0, 1, 0 ]);
+console.log(rowWeights([ 50, 60, 70, 80 ]), [ 120, 140 ]);
