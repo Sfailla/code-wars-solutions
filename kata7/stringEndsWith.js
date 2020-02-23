38b758930fbf06b1d803ebe8514a571c8dace787
Stevo99 <Sfailla1983@gmail.com>
2020-02-23 10:45:14 -0500

    solution for kata 7

---
+++ kata7/stringEndsWith.js
@@ -0,0 +1,19 @@
+/*
+
+Complete the solution so that it returns true if the first argument(string) 
+passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+*/
+
+function solution(str, ending) {
+	// TODO: complete
+	return str.slice(-ending.length) === ending;
+}
+
+console.log(solution('abcde', 'cde'), true);
+console.log(solution('abcde', 'abc'), false);
