c5b79498956fca28ac9989f46ec9bc2160dbb41b
Stevo99 <Sfailla1983@gmail.com>
2020-03-02 18:45:36 -0500

    solution for kata 7

---
+++ kata7/TheOffice-Sabbatical.js
@@ -0,0 +1,44 @@
+function sabb(x, val, happ) {
+	let letters = 'sabbatical';
+	const numbersToAdd = [];
+
+	const array = x.split('');
+	let mapped = array.reduce((obj, val) => {
+		val = val.toLowerCase();
+		obj[val] = obj[val] || 0;
+		obj[val]++;
+		return obj;
+	}, {});
+
+	for (const [ key, val ] of Object.entries(mapped)) {
+		if (letters.includes(key)) {
+			numbersToAdd.push(val);
+		}
+	}
+
+	const totalNums = [ ...numbersToAdd, val, happ ];
+	const total = totalNums.reduce((acc, curr) => acc + curr, 0);
+
+	return total > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.';
+}
+
+console.log(
+	sabb('Can I have a sabbatical?', 5, 5),
+	'Sabbatical! Boom!'
+);
+console.log(
+	sabb('Why are you shouting?', 7, 2),
+	'Back to your desk, boy.'
+);
+console.log(
+	sabb('What do you mean I cant learn to code??', 8, 9),
+	'Sabbatical! Boom!'
+);
+console.log(
+	sabb('Please calm down', 9, 1),
+	'Back to your desk, boy.'
+);
+console.log(
+	sabb('aotiiiczlioyrqldiacbz', 3, 6),
+	'Back to your desk, boy.'
+);
