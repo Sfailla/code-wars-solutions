5cf0a4962e41fd7862f32e60fa3684ea9cd6307a
Stevo99 <Sfailla1983@gmail.com>
2020-03-19 08:50:23 -0400

    solution for kata 7

---
+++ kata7/AbundantNumbers.js
@@ -0,0 +1,24 @@
+function abundantNumber(num) {
+	function getDivisableNums(number) {
+		let numToTotal = [];
+		for (let i = 1; i <= number; i++) {
+			if (number % i === 0 && i !== number) {
+				numToTotal.push(i);
+			}
+		}
+
+		return numToTotal;
+	}
+
+	const totalNum = getDivisableNums(num).reduce(
+		(acc, cur) => acc + cur,
+		0
+	);
+
+	return totalNum > num ? true : false;
+}
+
+console.log(abundantNumber(12), true);
+console.log(abundantNumber(37), false);
+console.log(abundantNumber(120), true);
+console.log(abundantNumber(77), false);
