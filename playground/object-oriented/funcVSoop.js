b22825076b16734b4198941e0d5d89113757586f
Stevo99 <Sfailla1983@gmail.com>
2020-03-25 21:22:32 -0400

    practice with classes and oop

---
+++ playground/object-oriented/funcVSoop.js
@@ -0,0 +1,55 @@
+class ShoppingList {
+	constructor(items) {
+		this.items = items;
+	}
+
+	addItem(item) {
+		this.items.push(item);
+	}
+
+	editItem(itemName, newItem) {
+		this.items.find(item => {
+			if (item.name === itemName) {
+				let editItem = {
+					name: newItem.name ? newItem.name : null,
+					price: newItem.price ? newItem.price : null
+				};
+				item.name =
+					editItem.name === null ? item.name : editItem.name;
+				item.price =
+					editItem.price === null ? item.price : editItem.price;
+			}
+		});
+	}
+
+	removeItem(itemName) {
+		let newArr = [];
+		this.items.filter(item => {
+			return item.name !== itemName ? newArr.push(item) : item;
+		});
+		this.items = newArr;
+	}
+}
+
+class ShoppingListItem {
+	constructor(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+}
+
+const myShoppingList = new ShoppingList([
+	new ShoppingListItem('milk', 4.99),
+	new ShoppingListItem('eggs', 3.99),
+	new ShoppingListItem('beer', 12.99)
+]);
+
+// adding a single item
+myShoppingList.addItem(new ShoppingListItem('cookies', 2.99));
+// removing a single item
+myShoppingList.removeItem('eggs');
+// edit a single item
+myShoppingList.editItem('milk', { name: 'rice', price: 1.99 });
+myShoppingList.editItem('cookies', { name: 'doritos', price: 5.99 });
+
+myShoppingList;
