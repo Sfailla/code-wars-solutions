4bf5ba3d790793318d3449be3bb2d6ef73bb3872
Stevo99 <Sfailla1983@gmail.com>
2020-06-01 09:53:59 -0400

    solution for kata 6

--- kata6/DuplicateArgs.js
+++ kata6/DuplicateArgs.js
@@ -1,7 +1,12 @@
 function solution(...args) {
-	const filtered = [ ...new Set(args) ].length;
+	const filtered = new Set(args).size;
 	return filtered === args.length ? false : true;
 }
+
+// ONE LINER CREATIVE SOLUTIONS
+// function solution(...args) {
+//   return new Set(args).size !== args.length;
+// }
 
 console.log(solution(1, 2, 3), false);
 console.log(solution(1, 2, 3, 6, 5, 6), true);
