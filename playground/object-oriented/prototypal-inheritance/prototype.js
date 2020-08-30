d877968464fc74316aa5bb7b5ce968ef2d4d3eae
Stevo99 <Sfailla1983@gmail.com>
2020-08-30 11:32:17 -0400

    added to prototype playground files. added personFactory that adds greet function to person prototype

--- playground/object-oriented/prototypal-inheritance/prototype.js
+++ playground/object-oriented/prototypal-inheritance/prototype.js
@@ -17,5 +17,49 @@ Object.setPrototypeOf(obj, protoObj);
 
 // now the lastName prop will be applied once it is found on the prototype chani
 console.log(obj.lastName);
-
 // javascript doesn't have classes so it uses prototypes instead
+// using prototype to save memory and cpu with factory functions
+// BAD
+function personFactory(firstName, lastName) {
+	return {
+		firstName,
+		lastName,
+		greet: function() {
+			return `Hello, ${firstName} ${lastName}`;
+		}
+	};
+}
+
+const steve = personFactory('Steven', 'Failla');
+const maria = personFactory('Maria', 'Failla');
+// although this works fine, it creates a new function/object everytime
+// its invoked. so not so good in larger applications
+steve.greet(); // Hello, Steven Failla
+maria.greet(); // Hello, Maria Failla
+steve.greet() === maria.greet(); // false... they are 2 diff objects
+
+// GOOD
+// to not pollute global namespace we can wrap in an IIFE
+let personFactory = (function() {
+	let personPrototype = {
+		greet: function(person) {
+			return `Hello, ${person.firstName} ${person.lastName}`;
+		}
+	};
+
+	return function(firstName, lastName) {
+		let person = Object.create(personPrototype, {
+			// we want read-only so writable: false
+			firstName: { writable: false, value: firstName },
+			lastName: { writable: false, value: lastName }
+		});
+
+		return person;
+	};
+})();
+
+const steve = personFactory('Steve', 'Failla');
+const maria = personFactory('Maria', 'Failla');
+
+console.log(steve.greet(steve), 'Hello, Steve Failla');
+console.log(steve.greet === maria.greet, true);
