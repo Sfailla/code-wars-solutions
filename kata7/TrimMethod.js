ace6c21d0309c3e485f454dd06d4ed46dcda30e5
Stevo99 <Sfailla1983@gmail.com>
2020-09-11 22:03:18 -0400

    working on solution for kata 7]

--- kata7/TrimMethod.js
+++ kata7/TrimMethod.js
@@ -1,15 +1,44 @@
 String.prototype.trim = function(c) {
 	// code goes here
-	let str = this.toLowerCase().split();
-	// let smallStr = c.toLowerCase();
+	const that = this;
 
-	return str;
+	let letters = Object.values(that.toLocaleLowerCase());
+
+	let i = 0;
+	let j = 1;
+	that;
+	letters;
+
+	while (i < letters.length) {
+		if (letters[i] === c) {
+			i++;
+		} else {
+			i;
+			letters = letters.slice(i);
+			break;
+		}
+
+		if (letters[letters.length - j] === c) {
+			j++;
+		} else {
+		}
+	}
+
+	letters;
+
+	// while ()
+
+	// return letters.filter(letter => {
+	// 	return letter !== c
+	// } )
+	// .replace(c.toLowerCase(), '');
+	// return that.replace(c.toLowerCase(), '');
 };
 
-let str = 'Hello Worldlll';
-str = str.trim('l');
-console.log(str);
+// let str = 'Hello Worldlll';
+// str = str.trim('l');
+// console.log(str);
 
 console.log('LLLHello Worldlll'.trim('l'), 'Hello World');
-console.log('   Hello World                 '.trim(), 'Hello World');
-console.log('XVisca ElbarcaXX'.trim('X'), 'Visca Elbarca');
+// console.log('   Hello World                 '.trim(), 'Hello World');
+// console.log('XVisca ElbarcaXX'.trim('X'), 'Visca Elbarca');
