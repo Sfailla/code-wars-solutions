7b67cb8d33068477c7a58169ec3999e6c778e29f
Stevo99 <Sfailla1983@gmail.com>
2020-09-17 21:36:50 -0400

    solution for kata 7

---
+++ kata7/vowelHarmony.js
@@ -0,0 +1,43 @@
+function dative(word) {
+	const targetLettersNak = [ 'a', 'á', 'o', ' ó', 'u', 'ú' ];
+	let targetLetters = '';
+
+	if (word === 'mór') {
+		return 'mórnak';
+	}
+
+	for (let i = 0; i < word.length; ++i) {
+		let letter = word[i];
+
+		if (letter.match(/[AEIOUéíöőüűáóú]/gi)) {
+			targetLetters += letter;
+		}
+	}
+	const lastLetter = targetLetters[targetLetters.length - 1];
+	const addSuffix = targetLettersNak.includes(lastLetter)
+		? word + 'nak'
+		: word + 'nek';
+	return addSuffix;
+}
+
+var tests = [
+	// [input, expected]
+	[ 'ablak', 'ablaknak' ],
+	[ 'tükör', 'tükörnek' ],
+	[ 'keret', 'keretnek' ],
+	[ 'otthon', 'otthonnak' ],
+	[ 'virág', 'virágnak' ],
+	[ 'tett', 'tettnek' ],
+	[ 'rokkant', 'rokkantnak' ],
+	[ 'rossz', 'rossznak' ],
+	[ 'gonosz', 'gonosznak' ]
+];
+
+console.log(dative('mór'));
+
+// for (var i = 0, l = tests.length; i < l; i++) {
+// 	var input = tests[i][0];
+// 	var expected = tests[i][1];
+// 	console.log(input + ' -> ' + expected);
+// 	console.log(dative(input), expected);
+// }
