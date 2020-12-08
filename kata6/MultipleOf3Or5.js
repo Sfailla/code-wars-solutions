02fac9fc9b054a571f0e917a0d6eacc98280bc74
Sfailla <Sfailla1983@gmail.com>
2020-12-08 14:34:09 -0500

    solution for kata 6

--- kata6/MultipleOf3Or5.js
+++ kata6/MultipleOf3Or5.js
@@ -1,19 +1,42 @@
 function solution(number) {
-	let multOf3 = [];
-	let multOf5 = [];
+	const range = number => ({
+		numberRange: Array(number - 1).fill().map((_, idx) => idx + 1)
+	});
 
-	for (let i = 1; i <= number; i++) {
-		console.log(i);
-		if (i % 3 === 0) {
-			multOf3.push(i);
-		} else if (i % 5 === 0) {
-			multOf5.push(i);
-		}
-	}
+	const getMultiples = array => ({
+		multiples: array.filter(num => num % 5 === 0 || num % 3 === 0)
+	});
 
-	console.log(multOf3);
-	console.log(multOf5);
-	return number;
+	const getTotal = array => ({ total: array.reduce((acc, cur) => acc + cur, 0) });
+
+	const { numberRange } = range(number);
+	const { multiples } = getMultiples(numberRange);
+	const { total } = getTotal(multiples);
+
+	return total;
 }
 
-console.log(solution(20)); // 23
+function solution(number) {
+	return number < 0
+		? 0
+		: Array(number - 1)
+				.fill()
+				.map((_, i) => i + 1)
+				.filter(num => num % 5 === 0 || num % 3 === 0)
+				.reduce((acc, cur) => acc + cur, 0);
+}
+
+// SMART SOLUTION
+
+function solution(number) {
+	let sum = 0;
+
+	for (let i = 0; i < number; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+console.log(solution(10)); // 3, 5, 6 and 9 added = 23;
