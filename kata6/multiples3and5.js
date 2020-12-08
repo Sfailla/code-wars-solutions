c35fb583f65fccc94b52e48488b9f9b26cfa7e6e
Sfailla <Sfailla1983@gmail.com>
2020-12-08 15:03:40 -0500

    still working

--- kata6/multiples3and5.js
+++ kata6/multiples3and5.js
@@ -1,27 +1,30 @@
 function solution(number) {
 	// multiples of 3 and 5
-	const requiredNumbers = [];
 
-	for (let i = 1; i < number; i++) {
+	const add = (a, b) => a + b;
+
+	let result = 0;
+
+	for (let i = 0; i < number; i++) {
 		const multipleOf3 = i % 3 === 0;
 		const multipleOf5 = i % 5 === 0;
 
 		if (multipleOf3 || multipleOf5) {
-			requiredNumbers.push(i);
+			result = add(result, i);
 		}
 	}
 
-	return requiredNumbers.reduce((acc, val) => acc + val, 0);
+	return result;
 }
 
-function solution(number) {
-	let answer;
-	for (let i = 1, arr = []; i < number; i++) {
-		i % 3 === 0 || i % 5 === 0 ? arr.push(i) : i;
-		i === number - 1 ? (answer = arr.reduce((acc, curr) => acc + curr, 0)) : 0;
-	}
-	return answer;
-}
+// function solution(number) {
+// 	let answer;
+// 	for (let i = 1, arr = []; i < number; i++) {
+// 		i % 3 === 0 || i % 5 === 0 ? arr.push(i) : i;
+// 		i === number - 1 ? (answer = arr.reduce((acc, curr) => acc + curr, 0)) : 0;
+// 	}
+// 	return answer;
+// }
 
 console.log(solution(10), 23);
 console.log(solution(20), 78);
@@ -30,3 +33,4 @@ console.log(solution(200), 9168);
 console.log(solution(1000), 233168);
 console.log(solution(10000), 23331668);
 console.log(solution(1000000), 233333166668);
+// console.log(solution(200000000), 9333333166666668);
