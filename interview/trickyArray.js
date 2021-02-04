5ae4d4f3fa24f6a0b203437e9a8f4478b1153eb1
Sfailla <Sfailla1983@gmail.com>
2021-02-03 21:18:04 -0500

    added tricky test question to kata code

--- interview/trickyArray.js
+++ interview/trickyArray.js
@@ -1,37 +1,57 @@
 const arr1 = [
-	[ 'name', 'id', 'age', 'weight' ],
-	[ 'Susan', '3', '10', '120' ],
-	[ 'John', '1', '11', '150' ],
-	[ 'Bob', '2', '23', '90' ],
-	[ 'Ben', '4', '20', '100' ]
+  ['name', 'id', 'age', 'weight'],
+  ['Susan', '3', '10', '120'],
+  ['John', '1', '11', '150'],
+  ['Bob', '2', '23', '90'],
+  ['Ben', '4', '20', '100'],
 ];
 
 const arr2 = [
-	[ 'name', 'id', 'height' ],
-	[ 'Bob', '2', '50' ],
-	[ 'John', '1', '45' ],
-	[ 'Ben', '4', '43' ],
-	[ 'Susan', '5', '48' ]
+  ['name', 'id', 'height'],
+  ['Bob', '2', '50'],
+  ['John', '1', '45'],
+  ['Ben', '4', '43'],
+  ['Susan', '3', '48'],
 ];
 
 const arr3 = [
-	[ 'name', 'id', 'parent' ],
-	[ 'Bob', '2', 'yes' ],
-	[ 'John', '1', 'yes' ]
+  ['name', 'id', 'parent'],
+  ['Bob', '2', 'yes'],
+  ['John', '1', 'yes'],
 ];
 
 function parseArray(arr) {
-	const [ headings, ...data ] = arr;
+  const [heading, ...items] = arr;
 
-	return data.reduce((acc, data, i) => {
-		const person = {};
+  const formatted = items.reduce((acc, data, i) => {
+    const person = {};
 
-		for (const [ index, heading ] of headings.entries()) {
-			person[heading] = data[index];
-		}
+    for (const [index, headings] of Object.entries(heading)) {
+      person[headings] = data[index];
+    }
 
-		return [ ...acc, person ];
-	}, []);
+    return [...acc, person];
+  }, []);
+
+  return formatted;
 }
 
-console.table(parseArray(arr1));
+const allData = [...parseArray(arr1), ...parseArray(arr2), ...parseArray(arr3)];
+
+const merge = allData.reduce((acc, currentPerson, i) => {
+  let existingPersonIndex = acc.findIndex(person => person.id === currentPerson.id);
+
+  if (existingPersonIndex >= 0) {
+    acc[existingPersonIndex] = {
+      ...acc[existingPersonIndex],
+      ...currentPerson,
+    };
+
+    return acc;
+  }
+
+  return [...acc, currentPerson];
+}, []);
+
+console.log(merge);
+console.log(parseArray(arr1));
